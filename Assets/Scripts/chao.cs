9872ee4074a2275843c978b66474a9ad7c4e973f
Gilnei Junior <gilnei.junior123@gmail.com>
2019-03-28 15:01:43 -0300

    init

---
+++ Assets/Scripts/chao.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chao : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2( - 0.5f * Time.deltaTime, 0));
+        transform.Rotate(new Vector3( 0, 0, -0.5f * Time.deltaTime));
+    }
+}
